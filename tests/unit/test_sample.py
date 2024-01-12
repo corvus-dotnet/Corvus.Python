505d145a5fca80a9bbbf39427811b4ae42b71090
Jonathan George <github@jonathangeorge.co.uk>
2024-01-12 10:57:15 +0000

    Fix linting issues

--- tests/unit/test_sample.py
+++ tests/unit/test_sample.py
@@ -1,10 +1,2 @@
-import spark_startup  # noqa F401
-from delta import DeltaTable
-from pyspark.sql import SparkSession # noqa E402
-
-spark = SparkSession.getActiveSession()
-db_name = spark.conf.get("default_database")
-
-
 def test_sample():
     assert True
