7c4e7f263fad2805fa9442ef6372efe1da77b5f5
Mike Evans-Larah <mike.evans-larah@endjin.com>
2024-01-26 17:36:08 +0000

    Add docstring

--- src/corvus_python/pyspark/utilities/null_safe_join.py
+++ src/corvus_python/pyspark/utilities/null_safe_join.py
@@ -15,18 +15,37 @@ def null_safe_join(
         broadcast_right_df: bool = False,
         left_alias: str = "left",
         right_alias: str = "right") -> DataFrame:
-
-    """Performs a join between two DataFrames, using the specified join columns and null-safe equality.
+    """
+    Performs a join between two DataFrames, using the specified join columns and null-safe equality.
     See https://spark.apache.org/docs/3.0.0-preview/sql-ref-null-semantics.html for more information on null
-    semantics and the null-safe equality operator (<=>) in Spark."""
+    semantics and the null-safe equality operator (<=>) in Spark.
 
-    # Using SQL syntax for the join conditions, which necessitates using the 'left' and 'right' aliases.
-    # The <=> operator is used as it is a null-safe equality operator, and we can expect null values.
+    :param left_df: The left DataFrame to join.
+    :type left_df: DataFrame
+    :param right_df: The right DataFrame to join.
+    :type right_df: DataFrame
+    :param join_columns: A list of tuples specifying the join columns.
+    :type join_columns: List[tuple]
+    :param left_cols_to_select: A list of column names to select from the left DataFrame.
+    :type left_cols_to_select: List[str]
+    :param right_cols_to_select: A list of column names to select from the right DataFrame.
+    :type right_cols_to_select: List[str]
+    :param join_how: The type of join to perform. Defaults to 'left'.
+    :type join_how: str
+    :param broadcast_right_df: Whether to broadcast the right DataFrame. Defaults to False.
+    :type broadcast_right_df: bool
+    :param left_alias: The alias for the left DataFrame. Defaults to 'left'.
+    :type left_alias: str
+    :param right_alias: The alias for the right DataFrame. Defaults to 'right'.
+    :type right_alias: str
+    :return: The resulting DataFrame after the join operation.
+    :rtype: DataFrame
+    """
+
     left_df = left_df.alias(left_alias)
     right_df = right_df.alias(right_alias)
     join_conditions = [f"{left_alias}.`{j[0]}` <=> {right_alias}.`{j[1]}`" for j in join_columns]
     join_condition = " AND ".join(join_conditions)
-
     select_cols = [left_df[i] for i in left_cols_to_select] + [right_df[i] for i in right_cols_to_select]
 
     if broadcast_right_df:
