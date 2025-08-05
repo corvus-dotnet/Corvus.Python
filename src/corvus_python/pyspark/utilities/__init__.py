3c3146123010a0e4b350c9f397ef0fce54d191b5
Mike Evans-Larah <mike.evans-larah@endjin.com>
2025-08-05 09:49:27 +0000

    Reorder imports

--- src/corvus_python/pyspark/utilities/__init__.py
+++ src/corvus_python/pyspark/utilities/__init__.py
@@ -5,7 +5,7 @@ except ImportError as exc:
     raise ImportError("PySpark is required for corvus-python PySpark utilities. "
                       "Install using corvus-python[pyspark]") from exc
 
+from .dataframe_utils import null_safe_join
 from .spark_utils.spark_utils import get_spark_utils
 from .session_utils.local_spark_session import LocalSparkSessionConfig, LocalSparkSession
 from .session_utils.get_or_create_spark_session import get_or_create_spark_session
-from .dataframe_utils import null_safe_join
