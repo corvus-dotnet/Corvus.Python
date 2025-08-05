6f0a6a66ddea96e1dc127d5e873a4cffeac9b578
Mike Evans-Larah <mike.evans-larah@endjin.com>
2025-08-05 12:37:06 +0000

    move PySpark import error handling

--- src/corvus_python/pyspark/synapse/__init__.py
+++ src/corvus_python/pyspark/synapse/__init__.py
@@ -1,8 +1,2 @@
 # flake8: noqa
-try:
-    import pyspark.sql
-except ImportError as exc:
-    raise ImportError("PySpark is required for corvus-python PySpark utilities. "
-                      "Install using corvus-python[pyspark]") from exc
-
 from .sync_tables_locally import sync_synapse_tables_to_local_spark, ObjectSyncDetails
--- src/corvus_python/pyspark/utilities/__init__.py
+++ src/corvus_python/pyspark/utilities/__init__.py
@@ -1,10 +1,4 @@
 # flake8: noqa
-try:
-    import pyspark.sql
-except ImportError as exc:
-    raise ImportError("PySpark is required for corvus-python PySpark utilities. "
-                      "Install using corvus-python[pyspark]") from exc
-
 from .dataframe_utils.null_safe_join import null_safe_join
 from .spark_utils.spark_utils import get_spark_utils
 from .session_utils.local_spark_session import LocalSparkSessionConfig, LocalSparkSession
