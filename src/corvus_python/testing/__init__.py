8a652f8f60292b280c8f292c01b7cb62b671206a
Mike Evans-Larah <mike.evans-larah@endjin.com>
2026-03-30 14:15:48 +0000

    Fix linting issues

--- src/corvus_python/testing/__init__.py
+++ src/corvus_python/testing/__init__.py
@@ -1,5 +1,11 @@
 from .polars_testing_utils import (
-    behave_table_to_polars_dataframe_with_inferred_schema,  # noqa: F401
-    behave_table_to_dictionary_by_row,  # noqa: F401
-    compare_polars_dataframes,  # noqa: F401
-)  # noqa: F401
+    behave_table_to_polars_dataframe_with_inferred_schema,
+    behave_table_to_dictionary_by_row,
+    compare_polars_dataframes,
+)
+
+__all__ = [
+    "behave_table_to_polars_dataframe_with_inferred_schema",
+    "behave_table_to_dictionary_by_row",
+    "compare_polars_dataframes",
+]
