2606bd9e87340bf4094be9dcce36f6bb93749607
Mike Evans-Larah <mike.evans-larah@endjin.com>
2026-03-30 13:03:38 +0000

    Fix import

--- src/corvus_python/synapse/synapse_utils.py
+++ src/corvus_python/synapse/synapse_utils.py
@@ -3,7 +3,7 @@ import urllib.parse
 from datetime import datetime, timedelta
 import time
 
-from ..monitoring.tracing import all_methods_start_new_current_span_with_method_name
+from corvus_python.monitoring.tracing import all_methods_start_new_current_span_with_method_name
 from opentelemetry import trace
 
 tracer = trace.get_tracer(__name__)
