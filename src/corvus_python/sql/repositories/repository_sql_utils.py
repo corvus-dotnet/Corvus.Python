a6d782e53dc27b444f2d571f76dcf2736b9cfe90
Mike Evans-Larah <2104606+MikeEvansLarah@users.noreply.github.com>
2026-04-23 10:40:21 +0100

    Add `repository_sql_utils` with `create_sql_views_from_tables` helper (#62)

---
+++ src/corvus_python/sql/repositories/repository_sql_utils.py
@@ -0,0 +1,31 @@
+from typing import Callable
+
+import pyodbc
+from ...sql import SelectColumn, create_or_alter_view_over_delta_table
+from ...repositories import TableDefinition
+
+
+def create_sql_views_from_tables(
+    conn: pyodbc.Connection,
+    schema_name: str,
+    tables: list[TableDefinition],
+    build_delta_table_path_func: Callable[[str], str],
+):
+    for table in tables:
+        if table.schema is None:
+            raise ValueError(f"Table {table.name} does not have a schema defined.")
+
+        select_cols: list[SelectColumn] = []
+        for name in table.schema.columns:
+            title = table.schema.columns[name].title
+            select_col = SelectColumn(name, title)
+            select_cols.append(select_col)
+
+        create_or_alter_view_over_delta_table(
+            conn,
+            schema_name,
+            view_name=table.name,
+            delta_table_path=build_delta_table_path_func(table.name),
+            infer_types=True,
+            select_columns=select_cols,
+        )
